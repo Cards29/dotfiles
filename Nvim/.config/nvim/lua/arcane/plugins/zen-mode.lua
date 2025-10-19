39481512094faa51ab69799e9243e49b50b7f26d
Rahinur Bin Naushad <rahinur2004@gmail.com>
2025-10-19 09:27:28 +0600

    zen mode settingsd

--- Nvim/.config/nvim/lua/arcane/plugins/zen-mode.lua
+++ Nvim/.config/nvim/lua/arcane/plugins/zen-mode.lua
@@ -21,12 +21,12 @@ return {
 			-- * an absolute number of cells when > 1
 			-- * a percentage of the width / height of the editor when <= 1
 			-- * a function that returns the width or the height
-			width = 120, -- width of the Zen window
+			width = 90, -- width of the Zen window
 			height = 1, -- height of the Zen window
 			-- by default, no options are changed for the Zen window
 			-- uncomment any of the options below, or add other vim.wo options you want to apply
 			options = {
-				-- signcolumn = "no", -- disable signcolumn
+				signcolumn = "no", -- disable signcolumn
 				-- number = false, -- disable number column
 				-- relativenumber = false, -- disable relative numbers
 				-- cursorline = false, -- disable cursorline
@@ -55,8 +55,8 @@ return {
 			-- - allow_remote_control socket-only
 			-- - listen_on unix:/tmp/kitty
 			kitty = {
-				enabled = true,
-				font = "+4", -- font size increment
+				enabled = false,
+				font = "+3", -- font size increment
 			},
 			-- this will change the font size on alacritty when in zen mode
 			-- requires  Alacritty Version 0.10.0 or higher
@@ -91,6 +91,7 @@ return {
 		},
 		-- callback where you can add custom code when the Zen window opens
 		on_open = function(win) end,
+
 		-- callback where you can add custom code when the Zen window closes
 		on_close = function() end,
 	},
