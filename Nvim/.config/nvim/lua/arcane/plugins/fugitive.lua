17fd9b6e17758eb5f86c8dbd7ad91e6f2950146a
Rahinur Bin Naushad <rahinur2004@gmail.com>
2025-10-20 12:17:02 +0600

    added undotree and fugitive

---
+++ Nvim/.config/nvim/lua/arcane/plugins/fugitive.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"tpope/vim-fugitive",
+		keys = {
+			{ "<leader>gs", "<cmd>Git<cr>", desc = "Fugitive: Git Status" },
+			{ "<leader>gb", "<cmd>Git blame<cr>", desc = "Fugitive: Git Blame" },
+			{ "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "Fugitive: Git Diffsplit" },
+		},
+	},
+}
