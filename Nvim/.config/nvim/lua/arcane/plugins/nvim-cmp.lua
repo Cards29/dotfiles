58f1409b6d7cf0df33aaeaad11ab0cc7238225c6
Rahinur Bin Naushad <rahinur2004@gmail.com>
2025-10-25 15:05:52 +0600

    <CR> not true

--- Nvim/.config/nvim/lua/arcane/plugins/nvim-cmp.lua
+++ Nvim/.config/nvim/lua/arcane/plugins/nvim-cmp.lua
@@ -41,7 +41,7 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
 				-- For Emmet completion
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
