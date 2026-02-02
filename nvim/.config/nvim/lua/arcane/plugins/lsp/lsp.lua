3affbd82c1f09495c7ee16707b602486ca1564a4
Rahinur Bin Naushad <rahinur2004@gmail.com>
2026-02-02 17:45:02 +0600

    no more vim undefined global
    copied aryan's lsp/lsp.lua file, dont know how it got fixed, must be the
    global block

--- nvim/.config/nvim/lua/arcane/plugins/lsp/lsp.lua
+++ nvim/.config/nvim/lua/arcane/plugins/lsp/lsp.lua
@@ -1,23 +1,35 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-		event = { "BufReadPre", "BufNewFile" },
-
-		config = function()
-			-- import cmp-nvim-lsp plugin
-			local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-			-- used to enable autocompletion (assign to every lsp server config)
-			local capabilities = cmp_nvim_lsp.default_capabilities()
-
-			vim.lsp.config("*", {
-				capabilities = capabilities,
-			})
-		end,
-	},
-
-	{
+	"hrsh7th/cmp-nvim-lsp",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
 		{ "antosha417/nvim-lsp-file-operations", config = true },
-		{ "folke/lazydev.nvim", opts = {} },
 	},
+	config = function()
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+		-- used to enable autocompletion (assign to every lsp server config)
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					runtime = {
+						version = "LuaJIT",
+					},
+					diagnostics = {
+						globals = { "vim" },
+					},
+					workspace = {
+						library = vim.api.nvim_get_runtime_file("", true),
+					},
+				},
+			},
+		})
+
+		vim.lsp.enable("lua_ls")
+
+		vim.lsp.config("*", {
+			capabilities = capabilities,
+		})
+	end,
 }
