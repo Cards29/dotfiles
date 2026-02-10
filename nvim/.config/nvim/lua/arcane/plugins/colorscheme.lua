d2870f1165d7a80ac6c62e6c632e3dd856e3b30a
Rahinur Bin Naushad <rahinur2004@gmail.com>
2026-02-10 21:07:20 +0600

    everforest it is

--- nvim/.config/nvim/lua/arcane/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/arcane/plugins/colorscheme.lua
@@ -1,32 +1,32 @@
 return {
 	{
-		"marko-cerovac/material.nvim",
-		lazy = false,
-		priority = 1000,
-		init = function()
-			vim.g.material_style = "deep ocean"
-		end,
-		opt = {
-			transparent = false,
-			term_colors = true,
+		"sainnhe/everforest",
+		priority = 1000, -- load before everything else
+		lazy = false, -- make sure it loads on startup
+		config = function()
+			-- Background: 'soft' (best for eyes), 'medium', 'hard'
+			vim.g.everforest_background = "soft"
 
-			styles = {
-				comments = { italic = true },
-				keywords = { italic = true },
-				functions = { bold = true, undercurl = true },
-				strings = {},
-				variables = {},
-				operators = {},
-				types = {},
-			},
+			-- Better performance & clean colors
+			vim.g.everforest_enable_italic = 1
+			vim.g.everforest_disable_italic_comment = 0
+			vim.g.everforest_transparent_background = 0
 
-			plugins = {
-				all = true,
-			},
-		},
-		config = function(_, opt)
-			require("material").setup(opt)
-			vim.cmd.colorscheme("material")
+			-- UI polish
+			vim.g.everforest_ui_contrast = "low"
+			vim.g.everforest_show_eob = 0 -- hide ~
+			vim.g.everforest_float_style = "dim" -- floating windows softer
+
+			-- Diagnostics & signs
+			vim.g.everforest_diagnostic_text_highlight = 1
+			vim.g.everforest_diagnostic_line_highlight = 0
+			vim.g.everforest_diagnostic_virtual_text = "grey"
+
+			-- Better current word visibility
+			vim.g.everforest_current_word = "grey background"
+
+			-- Apply theme
+			vim.cmd.colorscheme("everforest")
 		end,
 	},
 }
