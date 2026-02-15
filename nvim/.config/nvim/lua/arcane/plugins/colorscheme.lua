a391f852fc9899831b53bf063d574925c6347f78
Rahinur Bin Naushad <rahinur2004@gmail.com>
2026-02-15 13:05:41 +0600

    added a looooot of colors

--- nvim/.config/nvim/lua/arcane/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/arcane/plugins/colorscheme.lua
@@ -1,32 +1,41 @@
 return {
 	{
 		"sainnhe/everforest",
-		priority = 1000, -- load before everything else
-		lazy = false, -- make sure it loads on startup
+		priority = 1000,
+		lazy = false,
 		config = function()
-			-- Background: 'soft' (best for eyes), 'medium', 'hard'
 			vim.g.everforest_background = "soft"
-
-			-- Better performance & clean colors
 			vim.g.everforest_enable_italic = 1
 			vim.g.everforest_disable_italic_comment = 0
 			vim.g.everforest_transparent_background = 0
-
-			-- UI polish
 			vim.g.everforest_ui_contrast = "low"
-			vim.g.everforest_show_eob = 0 -- hide ~
-			vim.g.everforest_float_style = "dim" -- floating windows softer
-
-			-- Diagnostics & signs
+			vim.g.everforest_show_eob = 0
+			vim.g.everforest_float_style = "dim"
 			vim.g.everforest_diagnostic_text_highlight = 1
 			vim.g.everforest_diagnostic_line_highlight = 0
 			vim.g.everforest_diagnostic_virtual_text = "grey"
-
-			-- Better current word visibility
 			vim.g.everforest_current_word = "grey background"
 
-			-- Apply theme
 			vim.cmd.colorscheme("everforest")
 		end,
 	},
+
+	{ "catppuccin/nvim", name = "catppuccin", lazy = true },
+	{ "folke/tokyonight.nvim", lazy = true },
+	{ "rebelot/kanagawa.nvim", lazy = true },
+	{ "rose-pine/neovim", name = "rose-pine", lazy = true },
+	{ "sainnhe/gruvbox-material", lazy = true },
+	{ "EdenEast/nightfox.nvim", lazy = true },
+	{ "scottmckendry/cyberdream.nvim", lazy = true },
+	{ "AlexvZyl/nordic.nvim", lazy = true },
+	{ "olimorris/onedarkpro.nvim", lazy = true },
+	{ "nyoom-engineering/oxocarbon.nvim", lazy = true },
+	{ "savq/melange-nvim", lazy = true },
+	{ "kaicataldo/material.vim", lazy = true },
+	{ "mcchrish/zenbones.nvim", dependencies = { "rktjmp/lush.nvim" }, lazy = true },
+	{ "metalelf0/jellybeans-nvim", dependencies = { "rktjmp/lush.nvim" }, lazy = true },
+	{ "baliestri/aura-theme", src = "packages/neovim", lazy = true },
+	{ "shaunsingh/nord.nvim", lazy = true },
+	{ "bluz71/vim-moonfly-colors", name = "moonfly", lazy = true },
+	{ "oskarnurm/koda.nvim", lazy = true },
 }
