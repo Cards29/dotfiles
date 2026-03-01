8b61b3194c8e01e3d0e9a381894a1c09db0b87c1
Rahinur Bin Naushad <rahinur2004@gmail.com>
2026-03-01 19:50:50 +0600

    the new config format

--- nvim/.config/nvim/lua/plugins/nvim-colorizer.lua
+++ nvim/.config/nvim/lua/plugins/nvim-colorizer.lua
@@ -1,5 +1,19 @@
 return {
 	"catgoose/nvim-colorizer.lua",
 	event = "BufReadPre",
-	opts = {},
+
+	config = function()
+		require("colorizer").setup({
+			options = {
+				parsers = {
+					css = true, -- preset: enables names, hex, rgb, hsl, oklch
+					tailwind = { enable = true },
+				},
+				display = {
+					mode = "virtualtext",
+					virtualtext = { position = "after" },
+				},
+			},
+		})
+	end,
 }
