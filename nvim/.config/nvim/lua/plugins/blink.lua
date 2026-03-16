a3053d9e7bed7cfdecca5b00997ee5cf2827be19
Rahinur Bin Naushad <rahinur2004@gmail.com>
2026-03-16 19:34:53 +0600

    im an idiot, privider should be inside sources

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -50,14 +50,14 @@ return {
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
 			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-		},
 
-		providers = {
-			lazydev = {
-				name = "LazyDev",
-				module = "lazydev.integrations.blink",
-				-- make lazydev completions top priority (see `:h blink.cmp`)
-				score_offset = 100,
+			providers = {
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					-- make lazydev completions top priority (see `:h blink.cmp`)
+					score_offset = 100,
+				},
 			},
 		},
 
