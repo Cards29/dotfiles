99c6a8ea39920b0f9c94e67e1a94df76fe00f3c3
Rahinur Bin Naushad <rahinur2004@gmail.com>
2026-03-20 11:16:18 +0600

    formatting

--- nvim/.config/nvim/lua/plugins/git-blame.nvim.lua
+++ nvim/.config/nvim/lua/plugins/git-blame.nvim.lua
@@ -2,19 +2,21 @@
 -- Installed via store.nvim
 
 return {
-    "f-person/git-blame.nvim",
-    -- load the plugin at startup
-    event = "VeryLazy",
-    -- Because of the keys part, you will be lazy loading this plugin.
-    -- The plugin will only load once one of the keys is used.
-    -- If you want to load the plugin at startup, add something like event = "VeryLazy",
-    -- or lazy = false. One of both options will work.
-    opts = {
-        -- your configuration comes here
-        -- for example
-        enabled = true, -- if you want to enable the plugin
-        message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
-        date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
-        virtual_text_column = 1 -- virtual text start column, check Start virtual text at column section for more options
-    }
+	"f-person/git-blame.nvim",
+	event = "VeryLazy",
+	opts = {
+		enabled = true,
+		-- Refined template: removes extra brackets around SHA for a cleaner look
+		message_template = "  <summary> • <date> • <author> • <sha>",
+		date_format = "%Y-%m-%d %H:%M", -- Switched to ISO-style for better readability
+		virtual_text_column = 1,
+
+		-- Enhanced options
+		message_when_not_committed = "  Uncommitted changes",
+		highlight_group = "Comment", -- Keeps the blame subtle by matching your comments
+	},
+	keys = {
+		{ "<leader>gB", "<cmd>GitBlameToggle<cr>", desc = "Toggle Git Blame" },
+		{ "<leader>gO", "<cmd>GitBlameOpenCommitURL<cr>", desc = "Open Commit in Browser" },
+	},
 }
