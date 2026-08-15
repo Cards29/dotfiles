e7f93a11dc560ef8d201abc31beb736de509ed03
Rahinur Bin Naushad <codeberg_no1.bridged442@slmail.me>
2026-08-15 22:54:04 +0600

    nivm: blink selects the first suggestion when pressing enter

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -44,7 +44,16 @@ return {
 		},
 
 		-- (Default) Only show the documentation popup when manually triggered
-		completion = { documentation = { auto_show = false } },
+		completion = {
+			documentation = { auto_show = false },
+			-- Auto-select the first item when the menu appears
+			list = {
+				selection = {
+					preselect = true,
+					auto_insert = false, -- set to true if you want preview inserted into the buffer
+				},
+			},
+		},
 
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
