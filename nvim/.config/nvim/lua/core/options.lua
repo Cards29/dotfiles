20a5a23eb22906e4fc17be70a12bdee9eae20cf4
Rahinur Bin Naushad <codeberg_no1.bridged442@slmail.me>
2026-09-23 23:58:16 +0600

    misc: making stuff pretty

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -6,6 +6,7 @@ opt.relativenumber = true
 opt.number = true
 opt.updatetime = 50
 opt.fcs = "eob: "
+opt.cmdheight = 0
 
 -- tabs & indentation
 opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
